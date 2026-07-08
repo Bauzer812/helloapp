7656666afefd6d404cb730a02ea8310ca5c38135
white <white@example.com>
2026-07-08 20:13:22 +0500

    sasa

---
+++ hello-white — копия.py
@@ -0,0 +1,14 @@
+def say_hello():
+    print("hello everyone")
+
+def greetWhite():
+    print("hello white")
+
+def greetBlack():
+    print("hello black")
+
+if __name__ == "__main__":
+    say_hello()
+    greetWhite()
+    greetBlack()
+
